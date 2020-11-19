e48b331600a55f50e777ed0faf7d32539274774c
Julien-Blanchon <julien@blanchon.cc>
2020-11-19 21:33:38 +0100

    MAJ Probas

--- probas/tp/tp3/calcul_proba.m
+++ probas/tp/tp3/calcul_proba.m
@@ -6,7 +6,7 @@ function [x_min,x_max,probabilite] = calcul_proba(E_nouveau_repere,p)
     y_max = max(E_nouveau_repere(:, 2));
     l = abs(x_max - x_min);
     h = abs(y_max - y_min);
-    N = floor(l*h);
+    N = ceil(l*h);
     n = length(E_nouveau_repere);
     probabilite = 1 - binocdf(N, n-1, p);
 end
